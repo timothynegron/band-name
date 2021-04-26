cf95aab701afe71c0b1bb739e7cb147f09909a1d
Timothy Negron <timothynegron.eet@gmail.com>
2021-04-25 22:54:52 -0400

    Project Complete

--- band-name.py
+++ band-name.py
@@ -1,2 +1,4 @@
-print("Welcome to the Band Name Generator");
-input("What's the name of the city you grew up in?")
+print("Welcome to the Band Name Generator")
+cityName = input("What's the name of the city you grew up in?\n")
+petName = input("What is the name of your pet?\n")
+print("Your band name could be " + cityName + " " + petName)
